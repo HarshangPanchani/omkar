d001109b099b2e16da7617a4b19142c0a7c1e513
HARSHANG\panch <panchaniharshang@gmail.com>
2026-01-03 13:23:07 +0530

    test

---
+++ products.js
@@ -0,0 +1,29 @@
+/* OMKAR CERAMICS DATABASE */
+
+const products = [
+    {
+        "id": "1",
+        "name": "White Marble Floor Tile",
+        "category": "Floor Tiles",
+        "size": "600mm x 600mm",
+        "description": "Standard glossy white tile.",
+        "image": "images/tile1.jpg" 
+    },
+    {
+        "id": "2",
+        "name": "Matte Grey Wall Tile",
+        "category": "Wall Tiles",
+        "size": "300mm x 600mm",
+        "description": "Anti-skid matte finish.",
+        "image": "images/tile2.jpg"
+    },
+    {
+        "id": "tile-12345", 
+        "name": "Custom Granite Nameplate",
+        "category": "Nameplates",
+        "size": "12in x 6in",
+        "description": "Special order custom engraving.",
+        "image": "images/np_custom.jpg"
+    }
+    // NEXT AUTO ID WILL BE "3" (Because it ignores tile-12345)
+];

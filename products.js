177a7bc68cccf23ad4ce3a9595c4c80ec7ca2305
HARSHANG\panch <panchaniharshang@gmail.com>
2026-01-03 16:45:24 +0530

    test1

--- products.js
+++ products.js
@@ -7,23 +7,7 @@ const products = [
         "category": "Floor Tiles",
         "size": "600mm x 600mm",
         "description": "Standard glossy white tile.",
-        "image": "images/tile1.jpg" 
+        "image": "images/Orcalero-Orcala-Italian-Brainrot-768x715.avif" 
     },
-    {
-        "id": "2",
-        "name": "Matte Grey Wall Tile",
-        "category": "Wall Tiles",
-        "size": "300mm x 600mm",
-        "description": "Anti-skid matte finish.",
-        "image": "images/tile2.jpg"
-    },
-    {
-        "id": "tile-12345", 
-        "name": "Custom Granite Nameplate",
-        "category": "Nameplates",
-        "size": "12in x 6in",
-        "description": "Special order custom engraving.",
-        "image": "images/np_custom.jpg"
-    }
     // NEXT AUTO ID WILL BE "3" (Because it ignores tile-12345)
 ];
